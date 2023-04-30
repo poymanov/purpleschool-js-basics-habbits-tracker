68134000545eb18bf490786c5f5c9b2e10ba98e6
poymanov <n.poymanov@gmail.com>
2023-04-30 11:02:35 +0300

    Добавлены методы загрузки/сохранения привычек

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+/* Utils */
+
+function loadData() {
+	const habbitsString = localStorage.getItem(HABBIT_KEY);
+	const habbitsArray = JSON.parse(habbitsString);
+
+	if (Array.isArray(habbitsArray)) {
+		habbits = habbitsArray;
+	}
+}
+
+function saveData() {
+	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+	loadData();
+})();
